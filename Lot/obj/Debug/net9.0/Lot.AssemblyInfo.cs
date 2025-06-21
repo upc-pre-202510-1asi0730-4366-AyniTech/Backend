64c3e6b0f877025fadb75ed87597571297e05866
Fabs <fabiolasaldana2410@gmail.com>
2025-06-21 18:04:54 -0500

    pls

--- Lot/obj/Debug/net9.0/Lot.AssemblyInfo.cs
+++ Lot/obj/Debug/net9.0/Lot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f43e692b6457ac19096ddfac16f600250a4707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e465628af38ca7dfe68cb4cf7d30ab7e4f5fef5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
